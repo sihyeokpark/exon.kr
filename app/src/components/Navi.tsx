b3d331b99031d4477f3876277d43ab1d8a550957
시혁 박 <exon.indiv@gmail.com>
2022-08-01 22:22:18 +0900

    Update: Navi component

--- app/src/components/Navi.tsx
+++ app/src/components/Navi.tsx
@@ -1,9 +1,15 @@
 import logo from '../images/logo.png'
+import github from '../images/github.png'
 
 function Navi() {
     return (
         <nav>
-            <img src={logo} height="50vh"/>
+            <div className='left'>
+                <img src={logo} alt='logo' className='logo' height='50vh'/>
+            </div>
+            <div className='right'>
+                <img src={github} alt='github' className='github' height='30vh'/>
+            </div>
         </nav>
     )
 }
