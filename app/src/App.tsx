4ee74ae09ae1714e776681a97b345b7845b974c3
1-EXON <exon.indiv@gmail.com>
2022-08-17 22:33:24 +0900

    Update: Shadow Text "EXON"

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,8 +6,7 @@ function App() {
             <div className="container">
                 <section>
                     <div className="content">
-                        <h1>Hi!<br/>I'm induling in CODE</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum laudantium quam aspernatur eum nisi, blanditiis assumenda, aut dolor tempore, mollitia consequatur numquam quos corporis aliquid molestias iusto velit obcaecati id!</p>
+                        <h1 data-text="EXON">EXON</h1>
                     </div>
                 </section>
                 <section>
