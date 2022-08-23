ecc85671febe900aaa2940e2c37257241bace55f
1-EXON <exon.indiv@gmail.com>
2022-08-23 19:39:45 +0900

    Update: line shadow

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,27 +3,27 @@ import './css/App.css'
 function App() {
     return (
         <div>
-            <div className="container">
-                <section>
-                    <div className="content">
-                        <h1 data-text="EXON">EXON</h1>
+            <div className='container'>
+                <section className='dark'>
+                    <div className='content'>
+                        <h1 data-text='EXON'>EXON</h1>
                         <p>안녕하세요. 박시혁입니다.</p>
                     </div>
                 </section>
+                <section className='light'>
+                    <div className='content'>
+                        <h1 className='line'>도전하는 개발자.</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum laudantium quam aspernatur eum nisi, blanditiis assumenda, aut dolor tempore, mollitia consequatur numquam quos corporis aliquid molestias iusto velit obcaecati id!</p>
+                    </div>
+                </section>
                 <section>
-                    <div className="content">
+                    <div className='content'>
                         <h2>heleioafjowej</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum laudantium quam aspernatur eum nisi, blanditiis assumenda, aut dolor tempore, mollitia consequatur numquam quos corporis aliquid molestias iusto velit obcaecati id!</p>
                     </div>
                 </section>
                 <section>
-                    <div className="content">
-                        <h2>heleioafjowej</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum laudantium quam aspernatur eum nisi, blanditiis assumenda, aut dolor tempore, mollitia consequatur numquam quos corporis aliquid molestias iusto velit obcaecati id!</p>
-                    </div>
-                </section>
-                <section>
-                    <div className="content">
+                    <div className='content'>
                         <h2>heleioafjowej</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum laudantium quam aspernatur eum nisi, blanditiis assumenda, aut dolor tempore, mollitia consequatur numquam quos corporis aliquid molestias iusto velit obcaecati id!</p>
                     </div>
