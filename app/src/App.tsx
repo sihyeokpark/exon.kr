ecdc68409162ad9cf5d37c70de7a81f07c34ab3c
1-EXON <exon.indiv@gmail.com>
2022-08-26 20:32:32 +0900

    Update: page 2

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,16 +6,14 @@ function App() {
             <div className='container'>
                 <section className='dark'>
                     <div className='content'>
-                        <div className='main'>
-                            <h1 data-text='EXON'>EXON</h1>
-                            <p>안녕하세요. 박시혁입니다.</p>
-                        </div>
+                        <h1 data-text='EXON'>EXON</h1>
+                        <p>안녕하세요. 박시혁입니다.</p>
                     </div>
                 </section>
                 <section className='light'>
                     <div className='content'>
-                        <h1 className='line'>도전하는 개발자.</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum laudantium quam aspernatur eum nisi, blanditiis assumenda, aut dolor tempore, mollitia consequatur numquam quos corporis aliquid molestias iusto velit obcaecati id!</p>
+                        <h1>쓸데없어 보여도 쓸모 있다.</h1>
+                        <p>쓸데없는 것처럼 보이지만 누군가에겐 쓸모 있는 프로그램을 개발하고 있습니다.</p>
                     </div>
                 </section>
                 <section>
