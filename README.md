28d96d199602d887bc5051deeeb37cffd30e14f0
1-EXON <exon.indiv@gmail.com>
2022-12-29 23:14:12 +0900

    Update:

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Portfolio
 새로운 포트폴리오 사이트<br>
-<img src='./readmd/exon.kr.png' weight='100%'>
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,8 +6,20 @@ function App() {
             <div className='container'>
                 <section>
                     <p className='name'>SIHYEOK PARK</p>
-                    <h1 className='title'>for</h1>
+                    <h1 className='title'>Best Code for Better Life</h1>
                     <p>박시혁</p>
+                </section>
+                <section className='contents'>
+                    <p className='name'>교육/상</p>
+                    <h1 className='title'>Education / Awards</h1>
+                    <li>(2020~2022) 중앙기독중학교 14기</li>
+                    <li>(2022.12.28) 제 4회 한국코드페어 해커톤 중등부 <strong>대상(1등)</strong></li>
+                    <li>(2023~) 한국디지털미디어고등학교 <strong>해킹방어과 22기</strong></li>
+                </section>
+                <section className='contents'>
+                    <p className='name'>프로젝트</p>
+                    <h1 className='title'>Projects</h1>
+                    <li>(2022) 얼굴인식을 이용한 학생 출석 관리 프로그램</li>
                 </section>
             </div>
         </div>
