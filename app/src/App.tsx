e8e5925865b6674c2d034f5487fd8f544830ceb9
1-EXON <exon.indiv@gmail.com>
2023-01-24 17:52:02 +0900

    Update: main

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,8 +16,20 @@ function App() {
                     <img src={logo1} className='logo1'></img>
                     <img src={logo2} className='logo2'></img>
                 </div> */}
+                <section className='top'>
+                    <h1><span className='light' data-text={'Hi, I\'m Sihyeok Park —'}>Hi, I'm Sihyeok Park —</span><br/> <span className='light' data-text='known as EXON'>known as EXON</span></h1>
+                    <p className='intro'>
+                        I'm Hacker who is into Full-Stack Web, Hacking and Deeplearning.
+                        Currently {new Date().getFullYear() - 2007 + 1}-years-old({(() => {
+                        const date = new Date()
+                        return ((date.getMonth()+1 === 10 && date.getDate() >= 22) ? date.getFullYear() - 2007
+                            : (date.getMonth()+1 > 10) ? date.getFullYear() - 2007 : date.getFullYear() - 2007 - 1)
+                        })()} in Korean age).
+                        And I am a student at Korea Digital Media High School.
+                    </p>
+                </section>
                 <section>
-                    <h1>Hi, I'm Sihyeok Park — <br/> known as EXON</h1>
+                    <h1>Project</h1>
                     <p>
                         I'm Hacker who is into Full-Stack Web, Hacking and Deeplearning.
                         Currently {new Date().getFullYear() - 2007 + 1}-years-old({(() => {
@@ -26,7 +38,6 @@ function App() {
                             : (date.getMonth()+1 > 10) ? date.getFullYear() - 2007 : date.getFullYear() - 2007 - 1)
                         })()} in Korean age).
                     </p>
-                    
                 </section>
             </div>
         </>
