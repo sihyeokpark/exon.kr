11b86cfd53beaf3943ec2226efc86804e75eec61
1-EXON <exon.indiv@gmail.com>
2023-01-30 15:47:15 +0900

    Update: shadow

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -20,11 +20,11 @@ function App() {
                     </h1>
                     <p className='intro'>
                         I'm Hacker who is into Full-Stack Web, Hacking and Deeplearning.
-                        Currently {new Date().getFullYear() - 2007 + 1}-years-old({(() => {
+                        Currently {(() => {
                         const date = new Date()
                         return ((date.getMonth()+1 === 10 && date.getDate() >= 22) ? date.getFullYear() - 2007
                             : (date.getMonth()+1 > 10) ? date.getFullYear() - 2007 : date.getFullYear() - 2007 - 1)
-                        })()} in Korean age).
+                        })()}-years-old({new Date().getFullYear() - 2007 + 1} in Korean age).
                         And I am a student at Korea Digital Media High School.
                         I enjoy breaking challenges and thinking outside the box.
                     </p>
@@ -39,8 +39,27 @@ function App() {
                             </div>
                             <p>
                                 I won the 4th Korea-CodeFair Hackathon Grand Prize in Middle School Division with my team "Coding Without Hand", Wonkyu Go, Seungsoo Kim.
-                                We made project that can detect items in a convenience store and speak out the name of the item for the visually impaired.
-                                this project was made with Python, Tensorflow, and pure Javascript.
+                                We made <a href='https://github.com/codingWithoutHand/ConvenienceStoreForVisuallyImpaired' target='_blank' className='link'>project</a> that can detect items in a convenience store and speak out the name of the item for the visually impaired.
+                                this project was made with Python, Pytorch, Selenium, and pure Javascript.
+                            </p>
+                        </div>
+                        <div className='right'>
+                            <img src={codefair} className='codefair'></img>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div className='both'>
+                        <div className='left'>
+                            <h1>The 4th Korea-CodeFair <sup>2022.12</sup></h1>
+                            <div className='award'>
+                                <img src={award} height='30px'></img>
+                                <span>The 4th Korea-CodeFair Hackathon <strong>Grand Prize(1st)</strong> in Middle School Division</span>
+                            </div>
+                            <p>
+                                I won the 4th Korea-CodeFair Hackathon Grand Prize in Middle School Division with my team "Coding Without Hand", Wonkyu Go, Seungsoo Kim.
+                                We made <a href='https://github.com/codingWithoutHand/ConvenienceStoreForVisuallyImpaired' target='_blank' className='link'>project</a> that can detect items in a convenience store and speak out the name of the item for the visually impaired.
+                                this project was made with Python, Pytorch, Selenium, and pure Javascript.
                             </p>
                         </div>
                         <div className='right'>
